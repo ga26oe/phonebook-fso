b3dd056b96ff1f59bbf573b685d426956d92d10d
avi <gajai.avinash@gmail.com>
2026-03-12 07:32:35 -0400

    2.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = (props) => {
-  const [numbers, setNumbers] = useState(props.numbers) 
-  const [newName, setNewName] = useState('')
-
+  const [numbers, setNumbers] = useState(props.numbers);
+  const [newName, setNewName] = useState("");
 
   const addName = (event) => {
-    event.preventDefault()
-    const personObject ={
+    event.preventDefault();
+    const personObject = {
       name: newName,
+      id: newName,
+    };
+
+    const repeat = props.numbers.find((name) => name === setNewName)
+
+    if (!repeat) {
+    setNumbers(numbers.concat(personObject));
+    setNewName("");
+    console.log("button clicked", event.target);
+    } else {
+      alert(`${newName} has already been added.`)
     }
+  };
 
-    setNumbers(numbers.concat(personObject))
-    setNewName('')
-    console.log("button clicked", event.target)
-  }
-  
-
-    const handleNameChange = (event) => {
+  const handleNameChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
   };
-  
 
   return (
     <div>
@@ -31,26 +35,20 @@ const App = (props) => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button 
-           type="submit">add</button>
+          <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {numbers.map((number) => (
-          <li key={number.id}> 
-            {number.name}
-          </li>
+          <li key={number.id}>{number.name}</li>
         ))}
       </div>
 
-
-
       <div>debug: {newName}</div>
-        
-      </div>
-   
-  )
-}
+      
+    </div>
+  );
+};
 
-export default App
+export default App;
